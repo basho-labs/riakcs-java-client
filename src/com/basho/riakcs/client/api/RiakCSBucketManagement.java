ec9d906b81c1455ead7813b0893889a399d3a155
dillos <stephen.dillon@emc.com>
2014-09-05 15:47:15 +0100

    adding javadoc info

--- src/com/basho/riakcs/client/api/RiakCSBucketManagement.java
+++ src/com/basho/riakcs/client/api/RiakCSBucketManagement.java
@@ -2,18 +2,62 @@ package com.basho.riakcs.client.api;
 
 import org.json.JSONObject;
 
+/**
+ * RIAK CS client operating's for bucket management
+ */
 public interface RiakCSBucketManagement {
 
+	/**
+	 * List all buckets
+	 * 
+	 * @return JSON object with buck list
+	 */
 	public abstract JSONObject listBuckets();
 
+	/**
+	 * Create a bucket named with the passed strin
+	 * <p>
+	 * Bucket names must be unique TODO check what is throw when name is now unique and update doc
+	 * 
+	 * @param bucketName
+	 *            name of the new bucket
+	 */
 	public abstract void createBucket(String bucketName);
 
+	/**
+	 * Check if the current bucket is accessible
+	 * 
+	 * @param bucketName
+	 *            name of bucket to check
+	 * @return true if bucket is accessible
+	 */
 	public abstract boolean isBucketAccessible(String bucketName);
 
+	/**
+	 * TODO no idea, ACL is access control list maybe? need to update
+	 * <p>
+	 * need to add javadoc for ACL
+	 * 
+	 * @param bucketName
+	 *            name of bucket to retrieve ACL information for
+	 * @return Json object with ACL information
+	 */
 	public abstract JSONObject getACLForBucket(String bucketName);
 
+	/**
+	 * Delete a bucket, TODO add what happens if bucket doesnt exist or no permissions
+	 * 
+	 * @param bucketName
+	 *            name of bucket to delete
+	 */
 	public abstract void deleteBucket(String bucketName);
 
+	/**
+	 * Remove a bucket and delete all contents TODO how does this differ from delete???
+	 * 
+	 * @param bucketName
+	 *            name of the bucket to remove
+	 */
 	public abstract void removeBucketAndContent(String bucketName);
 
 }
--- src/com/basho/riakcs/client/api/RiakCSObjectManagement.java
+++ src/com/basho/riakcs/client/api/RiakCSObjectManagement.java
@@ -6,8 +6,27 @@ import java.util.Map;
 
 import org.json.JSONObject;
 
+/**
+ * Interface for RIAK CS object operations
+ * <p>
+ * TODO fill out rest of java docs when i know what in the JSON object
+ */
 public interface RiakCSObjectManagement {
 
+	/**
+	 * Save a object to RIAK CS
+	 * 
+	 * @param bucketName
+	 *            name of the bucket to save to
+	 * @param objectKey
+	 *            unique key for resource
+	 * @param dataInputStream
+	 *            input stream of the object to store
+	 * @param headers
+	 *            Map of headers to save
+	 * @param metadata
+	 *            meta data to save with the object
+	 */
 	public abstract void createObject(String bucketName, String objectKey, InputStream dataInputStream,
 			Map<String, String> headers, Map<String, String> metadata);
 
